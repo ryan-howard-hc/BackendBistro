1ff7a3e8edde27b1fed0dac3ffc61617113b495c
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-07-27 14:38:16 +0000

    ugh, postgres hates me

--- backendbistro/settings.py
+++ backendbistro/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backendbistro'
+    'menu.app.MenuConfig',
+    'backendbistro',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'backendbistro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    spicy_level = models.IntegerField()
+
+    def __str__(self):
+        return self.title
